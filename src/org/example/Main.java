85cc4afb4795991b208da8b5316ed211bdd1b91a
Таня Кишинская <lolo-20@yandex.ru>
2023-04-02 20:06:04 +0300

    доработка

---
+++ src/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int count;
+        //int count2 = 0;
+        Calculate calculate = new Calculate();
+        count = calculate.calc(10000, 3000);
+
+        System.out.println("Мы отдыхали " + count + " раз");
+
+    }
+
+}
