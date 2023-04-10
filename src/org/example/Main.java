b85810c0ab25f0a62684d8f7245dadb3c0c67ebe
Таня Кишинская <lolo-20@yandex.ru>
2023-04-10 16:25:12 +0300

    calculate2

--- src/org/example/Main.java
+++ src/org/example/Main.java
@@ -4,12 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         int count;
-        //int count2 = 0;
         org.example.Calculate calculate = new org.example.Calculate();
-        count = calculate.calc(10000, 3000);
-
+        count = calculate.calc(10000, 3000, 20_000);
         System.out.println("Мы отдыхали " + count + " раз");
 
+        System.out.println("====================");
+
+        count = calculate.calc(100_000, 60_000, 150_000);
+        System.out.println("Мы отдыхали " + count + " раз");
     }
 
 }
